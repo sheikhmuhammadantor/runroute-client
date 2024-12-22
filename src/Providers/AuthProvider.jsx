e861309752f81160cea89c59a04e476048c0fe90
Sheikh Muhammad Antor <sheikhmuhammadantor@gmail.com>
2024-12-22 12:53:14 +0600

    Add AuthProvider component for user authentication with Firebase.

---
+++ src/Providers/AuthProvider.jsx
@@ -0,0 +1,75 @@
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
+import { createContext, useEffect, useState } from 'react';
+import { auth } from '../Firebase/firebase';
+// import axios from 'axios';
+import PropTypes from 'prop-types';
+
+export const AuthContext = createContext(null);
+
+function AuthProvider({ children }) {
+
+    const [user, setUser] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const googleProvider = new GoogleAuthProvider();
+
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signInUser = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const googleSignIn = () => {
+        return signInWithPopup(auth, googleProvider)
+    }
+
+    const signOutUser = () => {
+        setLoading(true)
+        return signOut(auth);
+    }
+
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            if (currentUser.email) {
+                setUser(currentUser)
+            }
+            else {
+                setUser(null)
+            }
+            setLoading(false)
+            console.log('User', currentUser);
+        })
+
+        return () => {
+            unSubscribe()
+        }
+    }, []);
+
+    const authInfo = {
+        user,
+        email,
+        setEmail,
+        loading,
+        setLoading,
+        createUser,
+        signInUser,
+        googleSignIn,
+        signOutUser,
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+AuthProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+export default AuthProvider;
