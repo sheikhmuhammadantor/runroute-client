926fb4baa2f0e523bef4aa7e5a2b6122b068a81a
Sheikh Muhammad Antor <sheikhmuhammadantor@gmail.com>
2024-12-22 08:27:05 +0600

    SetUp Basic File, Start The Project.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl text-blue-600 font-semibold'>Hi, Sheikh Muhammad Antor @@</h1>
+    </>
+  )
+}
+
+export default App
