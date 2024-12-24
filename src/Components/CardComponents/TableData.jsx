851634857765760a70b8fd704aab56d8a549cdf0
Sheikh Muhammad Antor <sheikhmuhammadantor@gmail.com>
2024-12-24 09:01:13 +0600

    Enhance TableData component to conditionally display marathon start date and integrate MyApplyList for user applications.

--- src/Components/CardComponents/TableData.jsx
+++ src/Components/CardComponents/TableData.jsx
@@ -1,17 +1,25 @@
 import { ImCross } from "react-icons/im";
 import { MdEdit } from "react-icons/md";
 
-function TableData({ marathon, setMarathons, idx }) {
+function TableData({ marathon, setMarathons, idx, applies = false }) {
 
-    const { title, registrationStartDate, registrationEndDate, marathonStartDate, location, runningDistance, description, image, createdAt, totalRegistrations } = marathon;
+    const { title, marathonStartDate, location, runningDistance } = marathon;
     console.log(idx);
 
     return (
         <tr className="border-l-black border-l-2 text-center">
             <td className="border border-r-black border-r-2">{idx + 1}</td>
             <td className="border border-r-black border-r-2">{title}</td>
-            <td className="border border-r-black border-r-2">{location}</td>
-            <td className="border border-r-black border-r-2 hidden lg:table-cell">{runningDistance}</td>
+            {
+                applies ?
+                    <>
+                        <td className="border border-r-black border-r-2">{new Date(marathonStartDate).toLocaleDateString('en-GB', { day: '2-digit', month: 'short', year: 'numeric' })}</td>
+                    </> :
+                    <>
+                        <td className="border border-r-black border-r-2">{location}</td>
+                        <td className="border border-r-black border-r-2 hidden lg:table-cell">{runningDistance}</td>
+                    </>
+            }
             <td className="space-x-2 space-y-2 text-center border border-r-black border-r-2">
                 <button className="p-2 border rounded-full hover:border-transparent hover:text-white duration-150 hover:bg-teal-400"><MdEdit /></button>
                 <button className="p-2 border rounded-full hover:border-transparent hover:text-white duration-150 hover:bg-[#f70000]"><ImCross /></button>
--- src/Pages/MyPrivate/MyApplyList.jsx
+++ src/Pages/MyPrivate/MyApplyList.jsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from "react"
+import { TableData, useAuth, useAxios } from "../../App";
+
 function MyApplyList() {
+
+  const axiosInstance = useAxios();
+  const { user: { email } = {} } = useAuth();
+  const [applies, setApplies] = useState([]);
+
+  useEffect(() => {
+    axiosInstance.get(`/appliesByEmail/?email=${email}`)
+      .then(res => setApplies(res.data))
+      .catch(err => console.log(err))
+  }, [])
+
   return (
-    <div>
-      <h1>My Apply List</h1>
+    <div className="overflow-x-auto mt-8">
+      <table className="table w-full">
+        {/* head */}
+        <thead>
+          <tr>
+            <th className="text-center text-base text-black border border-b-black">ID</th>
+            <th className="text-center text-base text-black border border-b-black">Title</th>
+            <th className="text-center text-base text-black border border-b-black">Start Date</th>
+            <th className="text-center text-base text-black border border-b-black">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            applies?.map((marathon, idx) => <TableData key={idx} marathon={marathon} idx={idx} marathons={applies} setMarathons={setApplies} applies={true} />)
+          }
+        </tbody>
+      </table>
     </div>
   )
 }
