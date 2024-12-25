cee8fbe88dbd4bfdebe685c299f8394c1bf42d16
Sheikh Muhammad Antor <sheikhmuhammadantor@gmail.com>
2024-12-25 11:18:47 +0600

    Add modal functionality for updating marathon details and improve layout in Dashboard,

--- src/Components/CardComponents/TableData.jsx
+++ src/Components/CardComponents/TableData.jsx
@@ -1,9 +1,16 @@
+import { useEffect, useState } from "react";
 import { ImCross } from "react-icons/im";
 import { MdEdit } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 function TableData({ marathon, setMarathons, idx, handelDelete, applies = false }) {
 
-    const { _id, title, marathonStartDate, location, runningDistance } = marathon;
+    const { _id, title, registrationStartDate, registrationEndDate, marathonStartDate, location, runningDistance, description, image, createdAt, totalRegistrations } = marathon || {};
+    const [modal, setModal] = useState(null);
+
+    function formatDate(dateStr) {
+        return new Date(dateStr).toLocaleDateString('en-GB', { day: '2-digit', month: 'short', year: 'numeric' });
+    }
 
     const handelDeleteData = (id) => {
         handelDelete(id)
@@ -13,25 +20,55 @@ function TableData({ marathon, setMarathons, idx, handelDelete, applies = false 
             .catch(err => console.log(err))
     }
 
+    useEffect(() => {
+        setModal(document?.getElementById('my_modal_1'))
+    }, []);
+
+    const handelShowModal = () => {
+        modal?.showModal()
+    }
+
+    const handelUpdateDate = () => {
+        modal?.close()
+    }
+
     return (
-        <tr className="border-l-base-content border-l-2 text-center">
-            <td className="border border-base-content border-r-2">{idx + 1}</td>
-            <td className="border border-base-content border-r-2">{title}</td>
-            {
-                applies ?
+        <>
+            <tr className="border-l-base-content border-l-2 text-center">
+                <td className="border border-base-content border-r-2">{idx + 1}</td>
+                <td className="border border-base-content border-r-2">{title}</td>
+                {
+                    applies ?
+                        <>
+                            <td className="border border-base-content border-r-2">{new Date(marathonStartDate).toLocaleDateString('en-GB', { day: '2-digit', month: 'short', year: 'numeric' })}</td>
+                        </> :
+                        <>
+                            <td className="border border-base-content border-r-2">{location}</td>
+                            <td className="border border-base-content border-r-2 hidden lg:table-cell">{runningDistance}</td>
+                        </>
+                }
+                <td className="space-x-2 space-y-2 text-center border border-base-content border-r-2">
+                    <button onClick={handelShowModal} className="p-2 border rounded-full hover:border-transparent hover:text-white duration-150 hover:bg-teal-400"><MdEdit /></button>
+                    <button onClick={() => handelDeleteData(_id)} className="p-2 border rounded-full hover:border-transparent hover:text-white duration-150 hover:bg-[#f70000]"><ImCross /></button>
+                    {/* Dialog Box For Modal - */}
                     <>
-                        <td className="border border-base-content border-r-2">{new Date(marathonStartDate).toLocaleDateString('en-GB', { day: '2-digit', month: 'short', year: 'numeric' })}</td>
-                    </> :
-                    <>
-                        <td className="border border-base-content border-r-2">{location}</td>
-                        <td className="border border-base-content border-r-2 hidden lg:table-cell">{runningDistance}</td>
+                        <dialog id="my_modal_1" className="modal">
+                            <div className="modal-box max-w-max">
+                                <h3 className="font-bold text-lg">{title} - Updating</h3>
+                                <h1>hi</h1>
+                                <div className="modal-action">
+                                    <button onClick={() => handelUpdateDate()} className="btn">Update</button>
+                                    <form method="dialog">
+                                        <button className="btn mx-4">Close</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </dialog>
                     </>
-            }
-            <td className="space-x-2 space-y-2 text-center border border-base-content border-r-2">
-                <button className="p-2 border rounded-full hover:border-transparent hover:text-white duration-150 hover:bg-teal-400"><MdEdit /></button>
-                <button onClick={() => handelDeleteData(_id)} className="p-2 border rounded-full hover:border-transparent hover:text-white duration-150 hover:bg-[#f70000]"><ImCross /></button>
-            </td>
-        </tr>
+                </td>
+            </tr>
+
+        </>
     )
 }
 
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import Aside from "../Components/Dashboard/Aside"
 
 function Dashboard() {
     return (
-        <section className="grid grid-cols-12 gap-4 mb-12 items-start">
+        <section className="grid grid-cols-12 gap-4 mb-12 items-start max-w-[1380px] mx-auto">
             <Aside />
             <section className="col-span-12 lg:col-span-10">
                 <Outlet />
