6bfb33651bdf289ee56121d6629cb485c80746ad
Sheikh Muhammad Antor <sheikhmuhammadantor@gmail.com>
2024-12-26 12:07:12 +0600

    Implement pagination and item count retrieval in AllMarathons component.

--- src/Pages/Private/AllMarathons.jsx
+++ src/Pages/Private/AllMarathons.jsx
@@ -5,20 +5,46 @@ import { Helmet } from "react-helmet"
 function AllMarathons() {
 
   const [loading, setLoading] = useState(true);
-  const axiosInstance = useAxios();
   const [marathons, setMarathons] = useState([]);
+  const [itemCount, setItemCount] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemPerPage, setItemPerPage] = useState(9);
+  const axiosInstance = useAxios();
 
   useEffect(() => {
-    axiosInstance.get('/marathons')
+    axiosInstance.get(`/marathonCount`)
+      .then(res => {
+        setItemCount(res.data.count);
+      }).catch(err => console.log(err))
+  }, []);
+
+  useEffect(() => {
+    axiosInstance.get(`/marathons?skip=${(currentPage - 1) * itemPerPage}&limit=${itemPerPage}`)
       .then((data) => {
         setMarathons(data.data);
         setLoading(false);
       })
-  }, [])
+  }, [currentPage, itemPerPage])
 
-  if (loading) {
-    return <div className='text-3xl min-h-[70vh] grid place-items-center'><span className="loading loading-spinner text-info w-20"></span></div>
+  const numberOfPage = Math.ceil(itemCount / itemPerPage);
+  const pages = [...Array(numberOfPage).keys()];
+
+  const handelItemParPage = e => {
+    setItemPerPage(parseInt(e.target.value))
+    setCurrentPage(1)
   }
+
+  const handelPrevPage = () => {
+    if (currentPage > 1) setCurrentPage(currentPage - 1)
+  }
+
+  const handelNextPage = () => {
+    if (currentPage < numberOfPage) setCurrentPage(currentPage + 1)
+  }
+
+  // if (loading) {
+  //   return <div className='text-3xl min-h-[70vh] grid place-items-center'><span className="loading loading-spinner text-info w-20"></span></div>
+  // }
 
   return (
     <div className="m-8 max-w-[1380px] mx-auto">
@@ -32,6 +58,21 @@ function AllMarathons() {
         {
           marathons?.map((marathon, idx) => <DataCard key={idx} marathon={marathon} />)
         }
+      </div>
+      <div className="text-center flex justify-center gap-2 my-8">
+        <button onClick={handelPrevPage} className="btn btn-sm btn-outline px-6">Prev</button>
+        {
+          pages?.map(page =>
+            <button onClick={() => setCurrentPage(page + 1)} className={`btn btn-sm btn-outline ${currentPage === page + 1 ? 'bg-teal-500' : ''}`} key={page}>
+              {page + 1}
+            </button>)
+        }
+        <button onClick={handelNextPage} className="btn btn-sm btn-outline px-6">Next</button>
+        <select value={itemPerPage} onChange={handelItemParPage} className="btn btn-sm btn-outline btn-accent">
+          <option value="3">3</option>
+          <option value="6">6</option>
+          <option value="9">9</option>
+        </select>
       </div>
     </div>
   )
