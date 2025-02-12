e9deac44d4ac4dd96ae390c114f1e4d5d0c07933
Sheikh Muhammad Antor <sheikhmuhammadantor@gmail.com>
2025-02-12 07:02:37 +0600

    Refactor Navbar component for improved layout and user experience; replace logo with text and update asset references.

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,9 @@
-import logo from '../assets/footerLogo.png';
 import { NavLink, Link } from 'react-router-dom';
 import { useAuth } from '../App';
+import toast from 'react-hot-toast';
 import { useEffect, useState } from 'react';
 import { RiSunLine } from 'react-icons/ri'
 import { IoIosMoon } from 'react-icons/io'
-import toast from 'react-hot-toast';
 
 function Navbar() {
 
@@ -51,63 +50,63 @@ function Navbar() {
 
 
   return (
-    <section className='w-full bg-base-100 block fixed top-0 z-50 left-1/2 translate-x-[-50%]'>
-      <div className="navbar bg-base-100 max-w-[1535px] mx-auto px-6">
-        <div className="flex-1">
-          <Link to="/">
-            <img className='max-h-20 border border-dashed border-base-content rounded-md' src={logo} alt="" />
-          </Link>
-          <span onClick={handelChangeTheme} className="btn btn-sm text-2xl hidden md:block ml-2">
+    <div className="navbar bg-base-100/95 max-w-[1535px] mx-auto px-6">
+      <div className="flex-1">
+        <Link to="/" className=''>
+          <h2 className='text-center border border-base-content border-dashed px-4 py-0 rounded-md '>
+            <p className='text-2xl'>Run</p>
+            <p className='text-lg'>Route</p>
+          </h2>
+        </Link>
+        <span onClick={handelChangeTheme} className="btn btn-sm text-2xl hidden md:block ml-2">
             {
               (theme === "dark") ? <RiSunLine /> : <IoIosMoon />
             }
           </span>
-        </div>
-        <div className="flex-none">
-          <ul className="menu menu-horizontal px-1">
-            {/* <li><NavLink to="/">Home</NavLink></li> */}
-            <li><NavLink to="/marathons" className="sm:text-lg font-medium underline">Marathons</NavLink></li>
-            {
-              user ? <li><NavLink to="/dashboard/addMarathon" className="text-lg font-medium underline hidden sm:flex">Dashboard</NavLink></li> : ''
-            }
-            {links}
-          </ul>
-          <div>
-            {
-              user ?
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li><NavLink to="/marathons" className="sm:text-lg font-medium underline">Marathons</NavLink></li>
+          {
+            user ? <li><NavLink to="/dashboard/addMarathon" className="text-lg font-medium underline hidden sm:flex">Dashboard</NavLink></li> : ''
+          }
+          {links}
+        </ul>
+        <div>
+          {
+            user ?
+              <div className="relative group">
+                <label htmlFor="my-drawer-4" className="drawer-button btn px-1 outline-cyan-500 outline-2 outline outline-offset-1 border-none hover:outline-cyan-400 w-12 h-12 rounded-full overflow-hidden">
+                  <img className="max-w-full max-h-full" src={user?.photoURL} alt="" />
+                </label>
+                <div className="absolute top-12 right-0 bg-white shadow-lg border rounded w-60 p-2 hidden group-hover:block z-20">
+                  <ul>
+                    <li className="py-1 px-2 hover:bg-gray-200 cursor-pointer font-semibold">{user?.displayName}</li>
+                    <br />
+                    <li><NavLink to="/dashboard/addMarathon" className="btn btn-outline text-lg w-full">Dashboard</NavLink></li>
+                    {smLinks}
+                    <Link onClick={handelSignOut} className="btn btn-outline text-lg w-full">Log Out</Link>
+                  </ul>
+                </div>
+              </div> :
+              <div className='flex items-center'>
+                <Link to="/login" className="btn btn-sm sm:btn-md mr-1">Login</Link>
                 <div className="relative group">
                   <label htmlFor="my-drawer-4" className="drawer-button btn px-1 outline-cyan-500 outline-2 outline outline-offset-1 border-none hover:outline-cyan-400 w-12 h-12 rounded-full overflow-hidden">
-                    <img className="max-w-full max-h-full" src={user?.photoURL} alt="" />
+                    <img className="max-w-full max-h-full" src='/favicon.png' alt="" />
                   </label>
+                  <div></div>
                   <div className="absolute top-12 right-0 bg-white shadow-lg border rounded w-60 p-2 hidden group-hover:block z-20">
                     <ul>
-                      <li className="py-1 px-2 hover:bg-gray-200 cursor-pointer font-semibold">{user?.displayName}</li>
-                      <br />
-                      <li><NavLink to="/dashboard/addMarathon" className="btn btn-outline text-lg w-full">Dashboard</NavLink></li>
                       {smLinks}
-                      <Link onClick={handelSignOut} className="btn btn-outline text-lg w-full">Log Out</Link>
                     </ul>
                   </div>
-                </div> :
-                <div className='flex items-center'>
-                  <Link to="/login" className="btn btn-sm sm:btn-md mr-1">Login</Link>
-                  <div className="relative group">
-                    <label htmlFor="my-drawer-4" className="drawer-button btn px-1 outline-cyan-500 outline-2 outline outline-offset-1 border-none hover:outline-cyan-400 w-12 h-12 rounded-full overflow-hidden">
-                      <img className="max-w-full max-h-full" src='/favicon.png' alt="" />
-                    </label>
-                    <div></div>
-                    <div className="absolute top-12 right-0 bg-white shadow-lg border rounded w-60 p-2 hidden group-hover:block z-20">
-                      <ul>
-                        {smLinks}
-                      </ul>
-                    </div>
-                  </div>
                 </div>
-            }
-          </div>
+              </div>
+          }
         </div>
       </div>
-    </section>
+    </div>
   )
 }
 
